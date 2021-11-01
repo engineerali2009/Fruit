5c646a8e36c8833db48f0c0ea24587952ae973ca
Ali AbdelAziz <engineerali2009@gmail.com>
2021-11-01 11:35:57 +0200

    Initial commit

---
+++ src/Fruit.java
@@ -0,0 +1,80 @@
+public class Fruit {
+
+    private int var;
+   // int size=50;
+    //default constructor
+    public Fruit()
+    {
+       this.var = 10;
+    }
+    //parameterized constructor
+    public Fruit(int num)
+    {
+        this.var = num;
+    }
+    public int getValue()
+    {
+        return var;
+    }
+
+
+
+    ///////////////
+    static private int counter;
+    static  int counter2;
+
+    static public int getCounter() {return counter;}
+
+    /////////
+   public static int x=90;
+    public static int y=90;
+
+    String name;
+    int age;
+    int level;
+
+
+// Main Method declaration
+    public static void main(String[] args) {
+        int number = Fruit .getCounter();
+
+   // creating an object Mango from class fruit
+  Fruit Mango = new Fruit();
+  Fruit Apple = new Fruit();
+  Fruit Strawberry = new Fruit(100);
+
+
+        System.out.println("var is: "+Mango.getValue());
+        System.out.println("var is: "+Apple.getValue());
+        System.out.println("var is: "+Strawberry.getValue());
+
+
+
+        System.out.println("x Before swap "+x);
+
+        swap(x,y);
+        //System.out.println(swap(x,y));
+
+        System.out.println(x);
+        System.out.println("x After swap "+x);
+
+
+
+    }
+
+    int sugar_percentage(int a,int grams){return a*grams*100;}
+
+    int Fruit_Color(int a){return a;}
+
+    int Fruit_Type(int type){return type;}
+
+    ///// swap method declaration
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("These values printed from inside the method: "+a + ", " + b);
+
+    }
+
+}
